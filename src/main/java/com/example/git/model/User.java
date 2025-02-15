70a7049a31762e6ec678764ed3e89f0470e4e3b4
ranjitha1595 <ranjithasr32@1595>
2025-02-15 16:07:05 +0530

    adding user entity

---
+++ src/main/java/com/example/git/model/User.java
@@ -0,0 +1,31 @@
+package com.example.git.model;
+
+public class User {
+    private int id;
+    private String userName;
+    private int password;
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
